272f9d3c3d00ce6f1bb11d064483e567397c208f
Tolly Zhang <tollyzhang@gmail.com>
2025-10-12 13:50:16 -0700

    Fix typo in TimeController 'elapsed' attribute
    Renamed 'elapased' to 'elapsed' throughout the TimeController class for consistency and correctness.

--- src/handmotion/time_controller.py
+++ src/handmotion/time_controller.py
@@ -4,7 +4,7 @@ class TimeController:
     def __init__(self):
         self.start_time = 0
         self.current_time = 0
-        self.elapased = 0
+        self.elapsed = 0
         self.last_elapsed = 0
         self.delta = 0
     
@@ -13,15 +13,15 @@ class TimeController:
         
     def update(self):
         self.current_time = time.time_ns()
-        self.elapased = self.current_time - self.start_time
-        self.delta = self.elapased - self.last_elapsed
-        self.last_elapsed = self.elapased
+        self.elapsed = self.current_time - self.start_time
+        self.delta = self.elapsed - self.last_elapsed
+        self.last_elapsed = self.elapsed
     
     def get_elapsed_time_ns(self) -> float:
-        return self.elapased
+        return self.elapsed
 
     def get_elapsed_time_s(self) -> float:
-        return self.elapased / 1e9
+        return self.elapsed / 1e9
     
     def get_delta_ns(self) -> float:
         return self.delta
